1318176bdea57c1191e9ffeb2f89ca7a8eeba2f6
Samarth <Varahi1917115@gmail.com>
2022-09-07 21:32:10 -0400

    commit tree 9693497fc33d2123b15697c902d29ecdcdfc4b68

--- index.js
+++ index.js
@@ -1,25 +1,15 @@
+function reverseString(word) {
+  const wordArray = word.split ("");
+  const reversedWordArray = wordArray.reverse();
+  const reversedWord = reversedWordArray.join("");
+  return reversedWord;
+}
+
 function isPalindrome(word) {
-  // Write your algorithm here
+  const reversedWord = reverseString(word);
+  return word === reversedWord;
 }
 
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
-if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
-
-  console.log("");
-
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
+function reverseString(word) {
+  return word.split("").reverse().join("");
 }
-
-module.exports = isPalindrome;
